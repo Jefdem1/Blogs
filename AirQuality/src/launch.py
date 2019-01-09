88b696870630dc7afa555296c4359477550c8067
Jefdem1 <jefdem@gmail.com>
2019-01-09 01:43:17 +0000

    ugh fixed stuff

--- AirQuality/src/launch.py
+++
@@ -1,4 +0,0 @@
-import json
-
-test = "hello world"
-print(test)
--- AirQuality/src/sensor.py
+++ AirQuality/src/sensor.py
@@ -30,7 +30,7 @@ raw_adc = 0
 myAWSIoTMQTTClient = None
 myAWSIoTMQTTClient = AWSIoTMQTTClient('basicPubSub')
 myAWSIoTMQTTClient.configureEndpoint('a3shu6j73ct23v-ats.iot.us-east-1.amazonaws.com', '8883')
-myAWSIoTMQTTClient.configureCredentials('./root-CA.crt', './PiTemp1.private.key', './PiTemp1.cert.pem')
+myAWSIoTMQTTClient.configureCredentials('/home/pi/root-CA.crt', '/home/pi/PiTemp1.private.key', '/home/pi/PiTemp1.cert.pem')
 myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
 myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
 myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
@@ -46,121 +46,119 @@ def read(pin):
     return data
 
 def gettemp():
-	th = []
-	RPi.GPIO.setup(gpiopin, RPi.GPIO.OUT)
-	RPi.GPIO.output(gpiopin, RPi.GPIO.HIGH)
-	time.sleep(0.05)
-	RPi.GPIO.output(gpiopin, RPi.GPIO.LOW)
-	time.sleep(0.02)
-	RPi.GPIO.setup(gpiopin, RPi.GPIO.IN, RPi.GPIO.PUD_UP)
-	unchanged_count = 0
-	max_unchanged_count = 100
-	last = -1
-	data = []
-	while True:
-		current = RPi.GPIO.input(gpiopin)
-		data.append(current)
-		if last != current:
-			unchanged_count = 0
-			last = current
-		else:
-			unchanged_count += 1
-			if unchanged_count > max_unchanged_count:
-				break
+    th = []
+    RPi.GPIO.setup(gpiopin, RPi.GPIO.OUT)
+    RPi.GPIO.output(gpiopin, RPi.GPIO.HIGH)
+    time.sleep(0.05)
+    RPi.GPIO.output(gpiopin, RPi.GPIO.LOW)
+    time.sleep(0.02)
+    RPi.GPIO.setup(gpiopin, RPi.GPIO.IN, RPi.GPIO.PUD_UP)
+    unchanged_count = 0
+    max_unchanged_count = 100
+    last = -1
+    data = []
+    while True:
+        current = RPi.GPIO.input(gpiopin)
+        data.append(current)
+        if last != current:
+            unchanged_count = 0
+            last = current
+        else:
+            unchanged_count += 1
+            if unchanged_count > max_unchanged_count:
+                break
 
-	STATE_INIT_PULL_DOWN = 1
-	STATE_INIT_PULL_UP = 2
-	STATE_DATA_FIRST_PULL_DOWN = 3
-	STATE_DATA_PULL_UP = 4
-	STATE_DATA_PULL_DOWN = 5
-	state = STATE_INIT_PULL_DOWN
-	lengths = []
-	current_length = 0
-	for i in range(len(data)):
-	current = data[i]
-	current_length += 1
+    STATE_INIT_PULL_DOWN = 1
+    STATE_INIT_PULL_UP = 2
+    STATE_DATA_FIRST_PULL_DOWN = 3
+    STATE_DATA_PULL_UP = 4
+    STATE_DATA_PULL_DOWN = 5
+    state = STATE_INIT_PULL_DOWN
+    lengths = []
+    current_length = 0
+    for i in range(len(data)):
+        current = data[i]
+        current_length += 1
 
-	if state == STATE_INIT_PULL_DOWN:
-		if current == RPi.GPIO.LOW:
-			state = STATE_INIT_PULL_UP
-			continue
-		else:
-			continue
-	if state == STATE_INIT_PULL_UP:
-		if current == RPi.GPIO.HIGH:
-			state = STATE_DATA_FIRST_PULL_DOWN
-			continue
-		else:
-			continue
-	if state == STATE_DATA_FIRST_PULL_DOWN:
-		if current == RPi.GPIO.LOW:
-			state = STATE_DATA_PULL_UP
-			continue
-		else:
-			continue
-	if state == STATE_DATA_PULL_UP:
-		if current == RPi.GPIO.HIGH:
-			current_length = 0
-			state = STATE_DATA_PULL_DOWN
-			continue
-		else:
-			continue
-	if state == STATE_DATA_PULL_DOWN:
-		if current == RPi.GPIO.LOW:
-			lengths.append(current_length)
-			state = STATE_DATA_PULL_UP
-			continue
-		else:
-			continue
+        if state == STATE_INIT_PULL_DOWN:
+            if current == RPi.GPIO.LOW:
+                state = STATE_INIT_PULL_UP
+                continue
+            else:
+                continue
+        if state == STATE_INIT_PULL_UP:
+            if current == RPi.GPIO.HIGH:
+                state = STATE_DATA_FIRST_PULL_DOWN
+                continue
+            else:
+                continue
+        if state == STATE_DATA_FIRST_PULL_DOWN:
+            if current == RPi.GPIO.LOW:
+                state = STATE_DATA_PULL_UP
+                continue
+            else:
+                continue
+        if state == STATE_DATA_PULL_UP:
+            if current == RPi.GPIO.HIGH:
+                current_length = 0
+                state = STATE_DATA_PULL_DOWN
+                continue
+            else:
+                continue
+        if state == STATE_DATA_PULL_DOWN:
+            if current == RPi.GPIO.LOW:
+                lengths.append(current_length)
+                state = STATE_DATA_PULL_UP
+                continue
+            else:
+                continue
 
+    if len(lengths) != 40:
+        jeffresults = "missing data"
+        print("missing data")
+    else:
+        bits = []
+        shortest_pull_up = 1000
+        longest_pull_up = 0
 
-	if len(lengths) != 40:
-		jeffresults = "missing data"
-		print("missing data")
-	else:
-		bits = []
-		shortest_pull_up = 1000
-		longest_pull_up = 0
+        for i in range(0, len(lengths)):
+            length = lengths[i]
+            if length < shortest_pull_up:
+                shortest_pull_up = length
+            if length > longest_pull_up:
+                longest_pull_up = length
+        halfway = shortest_pull_up + (longest_pull_up - shortest_pull_up) / 2
+        bits = []
 
-		for i in range(0, len(lengths)):
-			length = lengths[i]
-			if length < shortest_pull_up:
-				shortest_pull_up = length
-			if length > longest_pull_up:
-				longest_pull_up = length
-		halfway = shortest_pull_up + (longest_pull_up - shortest_pull_up) / 2
-		bits = []
+        for i in range(0, len(lengths)):
+            bit = False
+            if lengths[i] > halfway:
+                bit = True
+            bits.append(bit)
+        
+        the_bytes = []
+        byte = 0
 
-		for i in range(0, len(lengths)):
-			bit = False
-			if lengths[i] > halfway:
-				bit = True
-			bits.append(bit)
-		
-		the_bytes = []
-		byte = 0
+        for i in range(0, len(bits)):
+            byte = byte << 1
+            if (bits[i]):
+                byte = byte | 1
+            else:
+                byte = byte | 0
+            if ((i + 1) % 8 == 0):
+                the_bytes.append(byte)
+                byte = 0
 
-		for i in range(0, len(bits)):
-			byte = byte << 1
-			if (bits[i]):
-				byte = byte | 1
-			else:
-				byte = byte | 0
-			if ((i + 1) % 8 == 0):
-				the_bytes.append(byte)
-				byte = 0
-
-		checksum = the_bytes[0] + the_bytes[1] + the_bytes[2] + the_bytes[3] & 255
-		if the_bytes[4] != checksum:
-			jeffresults = "Checksum Failed"
-			print(jeffresults)
-		else:
-			th["temp"] = (9.0 / 5.0 * int(the_bytes[2]) + 32)
-			th["humidity"] = the_bytes[0]
-		
-		return th
-			#print(9.0 / 5.0 * int(the_bytes[2]) + 32)
-			#print(the_bytes[0])
+        checksum = the_bytes[0] + the_bytes[1] + the_bytes[2] + the_bytes[3] & 255
+        if the_bytes[4] != checksum:
+            jeffresults = "Checksum Failed"
+            print(jeffresults)
+        else:
+            th.append((9.0 / 5.0 * int(the_bytes[2]) + 32))
+            th.append(the_bytes[0])
+    return th
+                #print(9.0 / 5.0 * int(the_bytes[2]) + 32)
+                #print(the_bytes[0])
 
 
 
@@ -183,6 +181,7 @@ Ro = val
 val = {}
 rs = 0.0
 def getgas():
+    gas = []
     raw_adc = 0
     rs = 0.0
     for i in range(READ_SAMPLE_TIMES):
@@ -195,31 +194,33 @@ def getgas():
 
     rs = rs/READ_SAMPLE_TIMES
     rr_ratio = rs/Ro
-    gas["GAS_LPG"] = (math.pow(10,(((math.log(rr_ratio)-LPGCurve[1])/LPGCurve[2])+LPGCurve[0])))
-    gas["CO"] = (math.pow(10,(((math.log(rr_ratio)-COCurve[1])/ COCurve[2]) + COCurve[0])))
-    gas["SMOKE"] = (math.pow(10,(((math.log(rr_ratio)-SmokeCurve[1])/SmokeCurve[2])+SmokeCurve[0])))
-	return gas
+    #GAS_LPG / CO / SMOKE
+    gas.append((math.pow(10,(((math.log(rr_ratio)-LPGCurve[1])/LPGCurve[2])+LPGCurve[0]))))
+    gas.append((math.pow(10,(((math.log(rr_ratio)-COCurve[1])/ COCurve[2]) + COCurve[0]))))
+    gas.append((math.pow(10,(((math.log(rr_ratio)-SmokeCurve[1])/SmokeCurve[2])+SmokeCurve[0]))))
+    return gas
 
 while True:
-	gas = []
-	th = []
-	while (len(gas) == 0):
-		gas = getgas()
-	while (len(th) == 0):
-		th = gettemp()
-	output = '{'
-	output += '"Temperature":"' + str(th["temp"]) + '",'
-	output += '"Humidity":"' + str(th["humidity"]) + '",'
-	output += '"TTL":' + str(int(time.time() + 86400)) + ','
-	output += '"Timestamp":' + str(int(time.time())) + ','
-	output += '"Location":"Outside",'
-	output += '"LPG":"' + str(gas["GAS_LPG"]) + '",'
-	output += '"CO":"' + str(gas["CO"]) + '",'
-	output += '"Smoke":"' + str(gas["SMOKE"]) + '"'
-	output += '}'
-	#iotresults = myAWSIoTMQTTClient.publish('sdk/test/Python', output, 1)
-	print(output + "(Posted: " + str(iotresults))
-	time.sleep(9)
+    gas = []
+    th = []
+    while (not gas):
+        gas = getgas()
+    while (not th):
+        th = gettemp()
+    output = '{'
+    output += '"Temperature":"' + str(th[0]) + '",'
+    output += '"Humidity":"' + str(th[1]) + '",'
+    output += '"TTL":' + str(int(time.time() + 86400)) + ','
+    output += '"Timestamp":' + str(int(time.time())) + ','
+    output += '"Location":"Outside",'
+    output += '"LPG":"' + str(gas[0]) + '",'
+    output += '"CO":"' + str(gas[1]) + '",'
+    output += '"Smoke":"' + str(gas[2]) + '"'
+    output += '}'
+    #iotresults = myAWSIoTMQTTClient.publish('sdk/test/Python', output, 1)
+    print(output)
+    #print("Posted: " + str(iotresults))
+    time.sleep(9)
 
 #getgas()
 #gettemp()
